80c649ccabcd3fd4de959b64d2a1a18c8f37799f
Phil Adams <philadams.net@gmail.com>
2014-09-29 12:14:02 -0400

    add linearlyScale func to Utility functions class

---
+++ app/src/main/java/edu/cornell/idl/meter/Utility.java
@@ -0,0 +1,14 @@
+package edu.cornell.idl.meter;
+
+/**
+ * Various utility functions.
+ */
+public class Utility {
+
+  public static float linearlyScale(float x, float oldMin, float oldMax, float newMin, float newMax) {
+    float oldRange = oldMax - oldMin;
+    float newRange = newMax - newMin;
+    return (((x - oldMin) * newRange) / oldRange) + newMin;
+  }
+
+}
