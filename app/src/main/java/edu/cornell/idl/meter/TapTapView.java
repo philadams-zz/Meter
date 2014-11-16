e02c726e1ec4a8b01140d0ec04c03ee9a347c7f8
Phil Adams <philadams.net@gmail.com>
2014-11-16 17:40:26 -0500

    show inc/dec indicator onTouch in TapTap

--- app/src/main/java/edu/cornell/idl/meter/TapTapView.java
+++ app/src/main/java/edu/cornell/idl/meter/TapTapView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -12,19 +13,17 @@ import android.view.View;
 /**
  * View that listens for taps: on the top half of the screen increment the value,
  * on the bottom decrement.
- * TODO: show a small 'out of 10' indicator in the meter, as well as in the instructions
- * TODO: indicate the two tappable regions (dotted boxes?)
  */
 public class TapTapView extends View {
 
   final static String TAG = "TapTapView";
   private int reportedValue;
-  private int minValue = 0;
-  private int maxValue = 10;
   private int initialValue = 5;
   private Paint reportedValuePaint;
   private String outOfTenText = "/10";
   private Paint outOfTenPaint;
+  private PointF activePointer;
+  private Paint activePointerPaint;
 
   public TapTapView(Context context) {
     this(context, null);
@@ -67,20 +66,35 @@ public class TapTapView extends View {
     outOfTenPaint.setTextAlign(Paint.Align.RIGHT);
     outOfTenPaint.setTextSize(0.1f);
     outOfTenPaint.setLinearText(true);
+
+    activePointerPaint = new Paint();
+    activePointerPaint.setAntiAlias(true);
+    activePointerPaint.setColor(Color.RED);
+    activePointerPaint.setTypeface(Typeface.SANS_SERIF);
+    activePointerPaint.setTypeface(Typeface.DEFAULT_BOLD);
+    activePointerPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+    activePointerPaint.setTextAlign(Paint.Align.RIGHT);
+    activePointerPaint.setTextSize(0.4f);
+    activePointerPaint.setLinearText(true);
   }
 
   /**
    * onTouchEvent()
    */
   public boolean onTouchEvent(MotionEvent motionEvent) {
+    float eventX = motionEvent.getX();
+    float viewWidth = getWidth();
     float eventY = motionEvent.getY();
     float viewHeight = getHeight();
     switch (motionEvent.getAction()) {
-      case MotionEvent.ACTION_DOWN:  // indicate user touching screen?
+      case MotionEvent.ACTION_DOWN:
+        activePointer = new PointF(eventX / viewWidth , eventY / viewHeight);
+        invalidate();
         return true;
       case MotionEvent.ACTION_MOVE:
         return true;
       case MotionEvent.ACTION_UP:
+        activePointer = null;
         handleTap(eventY / viewHeight);
         invalidate();
         return true;
@@ -97,9 +111,9 @@ public class TapTapView extends View {
     canvas.save(Canvas.MATRIX_SAVE_FLAG);
     canvas.scale((float) getWidth(), (float) getHeight());
 
-    //drawTappableAreas();
     drawReportedValue(canvas);
     drawOutOfTen(canvas);
+    drawActivePointer(canvas);
 
     canvas.restore();
   }
@@ -116,6 +130,16 @@ public class TapTapView extends View {
    */
   protected void drawReportedValue(Canvas canvas) {
     canvas.drawText(String.valueOf(reportedValue), 0.5f, 0.8f, reportedValuePaint);
+  }
+
+  /**
+   * show a plus or minus icon when the user taps the screen, depending on vertical position
+   */
+  protected void drawActivePointer(Canvas canvas) {
+    if (activePointer != null) {
+      String activePointerSymbol = (activePointer.y < 0.5) ? "+" : "-";
+      canvas.drawText(activePointerSymbol, activePointer.x, activePointer.y, activePointerPaint);
+    }
   }
 
   /**
