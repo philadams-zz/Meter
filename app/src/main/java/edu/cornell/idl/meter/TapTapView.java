5b8e84c814cd739a2807d10216b3ae2a7dfa317e
Phil Adams <philadams.net@gmail.com>
2014-11-20 18:23:46 -0500

    up/down tappable regions for TapTap

--- app/src/main/java/edu/cornell/idl/meter/TapTapView.java
+++ app/src/main/java/edu/cornell/idl/meter/TapTapView.java
@@ -19,11 +19,14 @@ public class TapTapView extends View {
   final static String TAG = "TapTapView";
   private int reportedValue;
   private int initialValue = 5;
+  private int ALPHA_MIN = 50;
+  private int ALPHA_MAX = 255;
   private Paint reportedValuePaint;
   private String outOfTenText = "/10";
   private Paint outOfTenPaint;
   private PointF activePointer;
-  private Paint activePointerPaint;
+  private Paint tapUpPaint;
+  private Paint tapDownPaint;
 
   public TapTapView(Context context) {
     this(context, null);
@@ -49,33 +52,37 @@ public class TapTapView extends View {
 
     reportedValuePaint = new Paint();
     reportedValuePaint.setAntiAlias(true);
-    reportedValuePaint.setColor(Color.BLUE);
+    reportedValuePaint.setColor(Color.parseColor("#000000"));
     reportedValuePaint.setTypeface(Typeface.SANS_SERIF);
     reportedValuePaint.setTypeface(Typeface.DEFAULT_BOLD);
     reportedValuePaint.setStyle(Paint.Style.FILL_AND_STROKE);
     reportedValuePaint.setTextAlign(Paint.Align.CENTER);
     reportedValuePaint.setTextSize(100f);
-    reportedValuePaint.setLinearText(true);
+    //reportedValuePaint.setLinearText(true);
 
     outOfTenPaint = new Paint();
     outOfTenPaint.setAntiAlias(true);
-    outOfTenPaint.setColor(Color.BLUE);
+    outOfTenPaint.setColor(Color.parseColor("#000000"));
     outOfTenPaint.setTypeface(Typeface.SANS_SERIF);
     outOfTenPaint.setTypeface(Typeface.DEFAULT_BOLD);
     outOfTenPaint.setStyle(Paint.Style.FILL_AND_STROKE);
     outOfTenPaint.setTextAlign(Paint.Align.RIGHT);
     outOfTenPaint.setTextSize(24);
-    outOfTenPaint.setLinearText(true);
+    //outOfTenPaint.setLinearText(true);
 
-    activePointerPaint = new Paint();
-    activePointerPaint.setAntiAlias(true);
-    activePointerPaint.setColor(Color.RED);
-    activePointerPaint.setTypeface(Typeface.SANS_SERIF);
-    activePointerPaint.setTypeface(Typeface.DEFAULT_BOLD);
-    activePointerPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-    activePointerPaint.setTextAlign(Paint.Align.RIGHT);
-    activePointerPaint.setTextSize(40);
-    activePointerPaint.setLinearText(true);
+    tapUpPaint = new Paint();
+    tapUpPaint.setColor(Color.parseColor("#EB6841"));
+    tapUpPaint.setAntiAlias(true);
+    tapUpPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+    tapUpPaint.setStrokeCap(Paint.Cap.SQUARE);
+    tapUpPaint.setAlpha(ALPHA_MIN);
+
+    tapDownPaint = new Paint();
+    tapDownPaint.setColor(Color.parseColor("#26ADE4"));
+    tapDownPaint.setAntiAlias(true);
+    tapDownPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+    tapDownPaint.setStrokeCap(Paint.Cap.SQUARE);
+    tapDownPaint.setAlpha(ALPHA_MIN);
   }
 
   /**
@@ -85,7 +92,6 @@ public class TapTapView extends View {
   public void onSizeChanged(int w, int h, int oldw, int oldh) {
     reportedValuePaint.setTextSize(0.9f * w);
     outOfTenPaint.setTextSize(0.2f * w);
-    activePointerPaint.setTextSize(0.4f * w);
   }
 
   /**
@@ -98,6 +104,7 @@ public class TapTapView extends View {
     switch (motionEvent.getAction()) {
       case MotionEvent.ACTION_DOWN:
         activePointer = new PointF(eventX, eventY);
+        highlightTapRegion(eventY / getHeight());
         invalidate();
         return true;
       case MotionEvent.ACTION_MOVE:
@@ -105,6 +112,7 @@ public class TapTapView extends View {
       case MotionEvent.ACTION_UP:
         activePointer = null;
         handleTap(eventY / getHeight());
+        lowlightTapRegion(eventY / getHeight());
         invalidate();
         return true;
       default:
@@ -119,14 +127,17 @@ public class TapTapView extends View {
   protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
 
-    //canvas.save(Canvas.MATRIX_SAVE_FLAG);
-    //canvas.scale(getWidth(), getHeight());
-
+    drawTapRegions(canvas);
     drawOutOfTen(canvas);
     drawReportedValue(canvas);
-    drawActivePointer(canvas);
+  }
 
-    //canvas.restore();
+  /**
+   * draw a rect to indicate a tappable region for incrementing reportedValue
+   */
+  protected void drawTapRegions(Canvas canvas) {
+    canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight() / 2 - 10, tapUpPaint);
+    canvas.drawRect(0, canvas.getHeight() / 2 + 10, canvas.getWidth(), canvas.getHeight(), tapDownPaint);
   }
 
   /**
@@ -148,21 +159,32 @@ public class TapTapView extends View {
   }
 
   /**
-   * show a plus or minus icon when the user taps the screen, depending on vertical position
-   */
-  protected void drawActivePointer(Canvas canvas) {
-    if (activePointer != null) {
-      String activePointerSymbol = (activePointer.y / canvas.getHeight() < 0.5) ? "+" : "-";
-      canvas.drawText(activePointerSymbol, activePointer.x, activePointer.y, activePointerPaint);
-    }
-  }
-
-  /**
    * If the tapPosition is less than .5, decrement reportedValue. Else, increment it.
    */
   protected void handleTap(float tapPosition) {
     int delta = (tapPosition < 0.5) ? 1 : -1;
     reportedValue = Utility.clamp(reportedValue + delta, 0, 10);
+  }
+
+  /**
+   * If there's an activePointer, make that tappable region completely opaque
+   */
+  protected void highlightTapRegion(float tapPosition) {
+    if (activePointer != null) {
+      if (tapPosition < 0.5) {
+        tapUpPaint.setAlpha(ALPHA_MAX);
+      } else {
+        tapDownPaint.setAlpha(ALPHA_MAX);
+      }
+    }
+  }
+
+  /**
+   * If there's an activePointer, make that tappable region completely opaque
+   */
+  protected void lowlightTapRegion(float tapPosition) {
+    tapUpPaint.setAlpha(ALPHA_MIN);
+    tapDownPaint.setAlpha(ALPHA_MIN);
   }
 
   /**
