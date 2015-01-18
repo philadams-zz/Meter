a7b74dca0031adf2858d2426915b6f3792b16631
Phil Adams <philadams.net@gmail.com>
2015-01-18 15:38:51 -0500

    display and interact with super vas bar without canvas scaling

--- app/src/main/java/edu/cornell/idl/meter/SuperVASNumberedView.java
+++ app/src/main/java/edu/cornell/idl/meter/SuperVASNumberedView.java
@@ -14,19 +14,22 @@ public class SuperVASNumberedView extends View {
 
   final static String TAG = "SuperVASView";
 
-  private Paint scaleSelectedPaint;
-  private Paint scaleUnselectedPaint;
-
+  // selector knob/circle
   private Paint selectorInnerPaint;
   private Paint selectorOuterPaint;
+  private float radius = 50;
 
-  private float minTargetY = 0.05f;
-  private float maxTargetY = 1.0f - minTargetY;
+  // scale line/bar thingy
+  private Paint scaleSelectedPaint;
+  private Paint scaleUnselectedPaint;
+  private float scaleX = 200;
+  private float scaleY1 = 0.0f;
+  private float scaleY2 = 400 - scaleY1;
+  private float selectorTargetY = 100f;
+  private float padding = radius;  // offset from the edges of the scale
 
-  private float scaleX = 0.5f;
-  private float scaleY1 = minTargetY;
-  private float scaleY2 = 1.0f - scaleY1;
-  private float selectorTargetY = 0.5f;
+  private float minTargetY = scaleY1 + padding + radius/2;
+  private float maxTargetY = scaleY2 + radius/2;
 
   public SuperVASNumberedView(Context context) {
     this(context, null);
@@ -47,13 +50,13 @@ public class SuperVASNumberedView extends View {
     scaleUnselectedPaint = new Paint();
     scaleUnselectedPaint.setAntiAlias(true);
     scaleUnselectedPaint.setStyle(Paint.Style.STROKE);
-    scaleUnselectedPaint.setStrokeWidth(0.005f);
+    scaleUnselectedPaint.setStrokeWidth(3);
     scaleUnselectedPaint.setColor(Color.LTGRAY);
 
     scaleSelectedPaint = new Paint();
     scaleSelectedPaint.setAntiAlias(true);
     scaleSelectedPaint.setStyle(Paint.Style.STROKE);
-    scaleSelectedPaint.setStrokeWidth(0.01f);
+    scaleSelectedPaint.setStrokeWidth(12);
     scaleSelectedPaint.setColor(getResources().getColor(android.R.color.holo_blue_light));
 
     selectorInnerPaint = new Paint();
@@ -64,31 +67,65 @@ public class SuperVASNumberedView extends View {
     selectorOuterPaint = new Paint();
     selectorOuterPaint.setAntiAlias(true);
     selectorOuterPaint.setStyle(Paint.Style.FILL);
-    selectorOuterPaint.setStrokeWidth(0.01f);
+    selectorOuterPaint.setStrokeWidth(4);
     selectorOuterPaint.setColor(getResources().getColor(android.R.color.holo_blue_light));
     selectorOuterPaint.setAlpha(125);  // must come after setColor()
   }
 
   /**
+   * onDraw
+   */
+  @Override
+  protected void onDraw(Canvas canvas) {
+    drawScaleBar(canvas);
+    drawSelectorKnob(canvas);
+  }
+
+  /**
+   * draw scale (vertical line)
+   * @param canvas
+   */
+  private void drawScaleBar(Canvas canvas) {
+    canvas.drawLine(scaleX, scaleY1, scaleX, selectorTargetY, scaleUnselectedPaint);
+    canvas.drawLine(scaleX, selectorTargetY, scaleX, scaleY2, scaleSelectedPaint);
+  }
+
+  /**
+   * draw selector (the little knob/circle thing)
+   * @param canvas
+   */
+  private void drawSelectorKnob(Canvas canvas) {
+    canvas.drawCircle(scaleX, selectorTargetY, radius, selectorOuterPaint);
+    canvas.drawCircle(scaleX, selectorTargetY, radius - 35, selectorInnerPaint);
+  }
+
+
+  /**
    * onTouchEvent()
    */
   public boolean onTouchEvent(MotionEvent motionEvent) {
-    float eventY = motionEvent.getY();
-    float viewHeight = getHeight();
     switch (motionEvent.getAction()) {
       case MotionEvent.ACTION_DOWN:  // indicate user touching screen?
         return true;
       case MotionEvent.ACTION_MOVE:
-        setSelectorTarget(eventY / viewHeight);
+        setSelectorTarget(motionEvent.getY());
         return true;
       case MotionEvent.ACTION_UP:
-        setSelectorTarget(
-            eventY / viewHeight);  // update the selector to point at where user clicked
+        setSelectorTarget(motionEvent.getY());  // update the selector to point at where user clicked
         return true;
       default:
         return super.onTouchEvent(motionEvent);
     }
   }
+
+  ///**
+  // * drawReportedValue(Canvas canvas)
+  // */
+  //protected void drawReportedValue(Canvas canvas) {
+  //  int xPos = canvas.getWidth() / 2;
+  //  int yPos = (int) ((canvas.getHeight() / 2) - getVerticalCenterOffsetForPaint(reportedValuePaint));
+  //  canvas.drawText(String.valueOf(reportedValue), xPos, yPos, reportedValuePaint);
+  //}
 
   private void setSelectorTarget(float target) {
     selectorTargetY = target;
@@ -99,27 +136,26 @@ public class SuperVASNumberedView extends View {
   }
 
   /**
-   * onDraw
+   * For a given (text) paint, compute it's vertical centering offset
+   */
+  protected float getVerticalCenterOffsetForPaint(Paint mPaint) {
+    return (mPaint.descent() + mPaint.ascent()) / 2;
+  }
+
+  /**
+   * onSizeChanged we update the font sizes, so we're scaling relative to the screen size
    */
   @Override
-  protected void onDraw(Canvas canvas) {
-    canvas.save(Canvas.MATRIX_SAVE_FLAG);
-    canvas.scale((float) getWidth(), (float) getHeight());
-
-    drawScale(canvas);
-    drawSelector(canvas);
-
-    canvas.restore();
-  }
-
-  private void drawScale(Canvas canvas) {
-    canvas.drawLine(scaleX, scaleY1, scaleX, selectorTargetY, scaleUnselectedPaint);
-    canvas.drawLine(scaleX, selectorTargetY, scaleX, scaleY2, scaleSelectedPaint);
-  }
-
-  private void drawSelector(Canvas canvas) {
-    canvas.drawCircle(0.5f, selectorTargetY, 0.04f, selectorOuterPaint);
-    canvas.drawCircle(0.5f, selectorTargetY, 0.015f, selectorInnerPaint);
+  public void onSizeChanged(int w, int h, int oldw, int oldh) {
+    scaleX = 0.2f * w;
+    scaleY1 = 0 + padding;
+    scaleY2 = h - padding;
+    selectorTargetY = h / 2;
+    minTargetY = scaleY1;
+    maxTargetY = scaleY2;
+    //reportedValuePaint.setTextSize(0.9f * w);
+    //outOfTenPaint.setTextSize(0.2f * w);
+    //plusMinusPaint.setTextSize(0.2f * w);
   }
 
   /**
